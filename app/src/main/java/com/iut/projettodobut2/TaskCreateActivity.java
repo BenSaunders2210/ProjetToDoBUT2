ae159572f207fc3a6c65d443d3ad38597bbec9a0
e2302783 <ben.saunders2210@gmail.com>
2026-04-03 10:55:55 +0200

    Début de creation de taches

--- app/src/main/java/com/iut/projettodobut2/TaskCreateActivity.java
+++ app/src/main/java/com/iut/projettodobut2/TaskCreateActivity.java
@@ -1,7 +1,15 @@
 package com.iut.projettodobut2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 /**
  *
@@ -11,6 +19,31 @@ public class TaskCreateActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_task);
+        Spinner spinner = (Spinner) findViewById(R.id.severite);
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.severite_array,
+                android.R.layout.simple_spinner_item
+        );
+// Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner.
+        spinner.setAdapter(adapter);
+
+        Button btnAjouter = (Button) findViewById(R.id.btn_ajout);
+        btnAjouter.setOnClickListener(v -> {
+            Intent intent = new Intent(TaskCreateActivity.this, TaskListActivity.class);
+            startActivity(intent);
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.parent_layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 
 }
